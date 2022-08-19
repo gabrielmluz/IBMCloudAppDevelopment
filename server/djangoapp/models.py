4dc63763150c8a8d0c058d33a266fe5e8b2d62e4
gabrielmluz <gabrielmluz21@gmail.com>
2022-08-19 20:47:49 -0300

    Create CarMake and CarModel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -10,6 +9,13 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -20,6 +26,22 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    CAR_TYPES = (
+        ('1', 'Sedan'),
+        ('2', 'SUV'),
+        ('3', 'Hatch'),
+        ('4', 'Other'),
+    )
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    dealer_id = models.CharField(max_length=50)
+    car_type =  models.CharField(max_length=1, choices=CAR_TYPES)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name + self.year
+    
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
